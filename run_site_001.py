4c571567238c87f57415a7d8a3fdf552e4d1602c
AyakashiLink <xzzlinkzzx@gmail.com>
2024-04-07 01:35:18 +0600

    Update run_site_001.py

--- run_site_001.py
+++ run_site_001.py
@@ -88,8 +88,8 @@ def read_json(file_path):
         return None
 
 # Считываем базу данных
-data_ml = read_json("data\\data_ml.json")
-data_gpt = read_json("data\\data_gpt.json")
+data_ml = read_json("data\data_ml.json")
+data_gpt = read_json("data\data_gpt.json")
 
 
 async def process_chunk(voice, text_chunk, output_file):
